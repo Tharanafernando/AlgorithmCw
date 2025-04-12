8a169a1fe36f0d4f687bc3700951d8aecba7d4b8
Tharanafernando <tharana.611@gmail.com>
2025-04-12 08:35:56 +0530

    commit changes

--- src/src/GraphPackage/Graph.java
+++ src/src/GraphPackage/Graph.java
@@ -1,4 +1,35 @@
 package GraphPackage;
 
 public class Graph {
+    int size;
+    int[] vertices;
+    int[][] adjacencyMatrix;
+
+    public Graph(int size) {
+        this.size = size;
+        vertices = new int[size];
+        adjacencyMatrix = new int[size][size];
+    }
+
+
+    public void addVertex(int v,int u){
+        boolean vExists = false;
+        boolean uExists = false;
+        for (int i = 0; i < vertices.length; i++) {
+           if (uExists == true){
+
+           }
+           vertices[i]=v;
+           vertices[i++] = u;
+           uExists = true;
+
+
+        }
+
+
+
+    }
+    public void addEdge(int source,int destination,int weight){
+
+    }
 }
--- src/src/Main.java
+++ src/src/Main.java
@@ -1,26 +1,36 @@
+import GraphPackage.Graph;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args) {
+        Graph graph = null;
         String filepath = "benchmarkFiles/bridge_1.txt";
 
         try(BufferedReader br = new BufferedReader(new FileReader(filepath))){
             String line;
 
             while ((line = br.readLine()) != null) {
+
                 String[] parts = line.trim().split("\\s+");
                 if(parts.length == 3){
                     int node1 = Integer.parseInt(parts[0]);
                     int node2 = Integer.parseInt(parts[1]);
                     int weight = Integer.parseInt(parts[2]);
-                    System.out.println(node1 + " " + node2 + " " + weight);
+                    graph.addVertex(node1,node2);
+                    graph.addEdge(node1,node2,weight);
+
+//                    System.out.println(node1 + " " + node2 + " " + weight);
                 }else if(parts.length == 1){
                     int numberOfNodes = Integer.parseInt(parts[0]);
-                    System.out.println(numberOfNodes);
+                    graph = new Graph(numberOfNodes);
+//                    System.out.println(numberOfNodes);
                 }
+
 
             }
         } catch (FileNotFoundException e) {
