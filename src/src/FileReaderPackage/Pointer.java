093f736728c32b033b441568ea79709bd348dbe5
Tharanafernando <tharana.611@gmail.com>
2025-04-26 05:50:00 +0530

    Add file pointer class to read data

---
+++ src/src/FileReaderPackage/Pointer.java
@@ -0,0 +1,79 @@
+package FileReaderPackage;
+
+import GraphPackage.Graph;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Pointer {
+
+
+    public void readFile(String filepath) {
+        Graph graph = null;
+        //String filepath = "benchmarkFiles/bridge_15.txt";
+        //String filepath = "benchmarkFiles/ladder_1.txt";
+        int numberOfNodes = 0;
+
+        ArrayList<Integer> nodes = new ArrayList<>();
+        try(
+                BufferedReader br = new BufferedReader(new FileReader(filepath))){
+            String line;
+
+
+            while ((line = br.readLine()) != null) {
+
+                String[] parts = line.trim().split("\\s+");
+                if(parts.length == 3){
+                    int node1 = Integer.parseInt(parts[0]);
+                    int node2 = Integer.parseInt(parts[1]);
+                    int weight = Integer.parseInt(parts[2]);
+                    nodes.add(node1);
+                    nodes.add(node2);
+
+
+
+                    graph.addVertex(node1);
+                    graph.addVertex(node2);
+                    graph.addEdge(node1,node2,weight);
+
+
+
+                }else if(parts.length == 1){
+                    numberOfNodes = Integer.parseInt(parts[0]);
+
+                    graph = new Graph(numberOfNodes);
+                }
+
+
+
+
+            }
+            if(graph != null){
+                System.out.println("DFS path starting from node 0: ");
+
+                int maxValue = Collections.max(nodes);
+                int minValue = Collections.min(nodes);
+                int destination = numberOfNodes-1;
+                System.out.println(maxValue);
+                System.out.println(minValue);
+                //graph.fordFulkerson(0,destination);
+                graph.edmondsKarp(0,destination);
+
+                System.out.println();
+                // graph.printGraph();
+
+            }
+
+
+        } catch (
+                IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
--- src/src/Main.java
+++ src/src/Main.java
@@ -1,3 +1,4 @@
+import FileReaderPackage.Pointer;
 import GraphPackage.Graph;
 import GraphPackage.Node;
 
@@ -12,65 +13,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Graph graph = null;
-        String filepath = "benchmarkFiles/bridge_15.txt";
-        //String filepath = "benchmarkFiles/ladder_1.txt";
-        int numberOfNodes = 0;
-
-        ArrayList<Integer>nodes = new ArrayList<>();
-        try(BufferedReader br = new BufferedReader(new FileReader(filepath))){
-            String line;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the path of the file");
+        System.out.println("Example - 'benchmarkFiles/bridge_15.txt'");
+        String filepath = scanner.nextLine();
+        Pointer pointer = new Pointer();
+        pointer.readFile(filepath);
 
 
-            while ((line = br.readLine()) != null) {
-
-                String[] parts = line.trim().split("\\s+");
-                if(parts.length == 3){
-                    int node1 = Integer.parseInt(parts[0]);
-                    int node2 = Integer.parseInt(parts[1]);
-                    int weight = Integer.parseInt(parts[2]);
-                    nodes.add(node1);
-                    nodes.add(node2);
-
-
-
-                    graph.addVertex(node1);
-                    graph.addVertex(node2);
-                    graph.addEdge(node1,node2,weight);
-
-
-
-                }else if(parts.length == 1){
-                    numberOfNodes = Integer.parseInt(parts[0]);
-
-                    graph = new Graph(numberOfNodes);
-                }
-
-
-
-
-            }
-            if(graph != null){
-                System.out.println("DFS path starting from node 0: ");
-
-                int maxValue = Collections.max(nodes);
-                int minValue = Collections.min(nodes);
-                int destination = numberOfNodes-1;
-                System.out.println(maxValue);
-                System.out.println(minValue);
-                //graph.fordFulkerson(0,destination);
-                graph.edmondsKarp(0,destination);
-
-                System.out.println();
-               // graph.printGraph();
-
-            }
-
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
