4ad9ea89d3a8b00b03b98d10290b578bc4140955
Tharanafernando <tharana.611@gmail.com>
2025-04-26 05:55:00 +0530

    make changes to main class to retake data after complete a file once

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,24 +1,29 @@
 import FileReaderPackage.Pointer;
-import GraphPackage.Graph;
-import GraphPackage.Node;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
+
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the path of the file");
-        System.out.println("Example - 'benchmarkFiles/bridge_15.txt'");
-        String filepath = scanner.nextLine();
-        Pointer pointer = new Pointer();
-        pointer.readFile(filepath);
+        boolean continueLoop = true;
+        while (continueLoop) {
+            System.out.println("Enter the path of the file");
+            System.out.println("Example - 'benchmarkFiles/bridge_15.txt'");
+            String filepath = scanner.nextLine();
+            Pointer pointer = new Pointer();
+            pointer.readFile(filepath);
+
+            System.out.println("Want to run another file: Enter 'C' to continue OR enter 'E' to exit");
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("E")) {
+                continueLoop = false;
+            }
+
+        }
+
 
 
     }
